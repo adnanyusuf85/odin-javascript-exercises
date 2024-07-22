84b3766f4033bca6e244cf89dd29f2bdfdaac3fd
adnanyusuf85 <adnan.yusufs@gmail.com>
2024-07-22 18:00:33 +0500

    Fix repeatString code to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputText,numOfIterations) {
+    let workingString = "";
+    if (numOfIterations < 0)
+    {
+        return "ERROR";
+    }
+    for (let i=0; i<numOfIterations;i++)
+    {
+        workingString += inputText;
+    }
+    return workingString;
 };
 
 // Do not edit below this line
