61e387092b3738986190f846e1e622f6fcc1b698
Adnan Yusuf <adnanyusuf@outlook.com>
2024-07-25 05:52:16 +0500

    Add code to pass test successfully for reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    const lengthOfInputString = inputString.length;
+    let reversedString = "";
+    for (let i=lengthOfInputString;i>0;i--)
+    {
+        reversedString += inputString[i-1];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
