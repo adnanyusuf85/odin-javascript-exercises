5da1aebb0459c9913c905f73450ca63651c101ef
Adnan Yusuf <adnanyusuf@outlook.com>
2024-07-25 06:40:24 +0500

    Add lines to function to pass removeFromArray.js tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
 const removeFromArray = function() {
+    let inputArray = arguments[0];
+    let processedArray = [];
+    totalNumbersToRemove = arguments.length-1;
+    
+    const numbersToRemove = Array.from(arguments).slice(1);
+    console.log(numbersToRemove.toString());
+    inputArray.forEach(element => {
+        if (!numbersToRemove.includes(element))
+        {
+            processedArray.push(element);
+        }
+    });
+    console.log(`output array ${processedArray.toString()}`);
+    return processedArray;
 };
 
 // Do not edit below this line
