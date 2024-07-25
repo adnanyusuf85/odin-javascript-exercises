9276bea3f09d29e898d44c275cb983787a3e8e21
Adnan Yusuf <adnanyusuf@outlook.com>
2024-07-25 07:24:26 +0500

    Add Code to pass tests for sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,39 @@
 const sumAll = function() {
+    if (!validateNumber(arguments[0]) || !validateNumber(arguments[1]))
+        return "ERROR";
 
+
+    let smallNumber = Math.min(arguments[0],arguments[1]);
+    let largeNumber = Math.max(arguments[0],arguments[1]);
+    let sum = 0;
+    for (let i = smallNumber; i<= largeNumber; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
+
+
+
+function validateNumber(num)
+{
+    return validateIsANumber(num) && validateIsAnInteger(num) && validateIsPositive(num);
+}
+
+function validateIsANumber(num)
+{
+    return !isNaN(num);
+}
+
+function validateIsAnInteger(num)
+{
+    return Number.isInteger(num);
+}
+
+function validateIsPositive(num)
+{
+    return num >= 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
