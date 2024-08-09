e6019d1ea1eeaddc5a99d6feff974bdf32e604ec
Adnan Yusuf <adnanyusuf@outlook.com>
2024-08-09 07:41:15 +0500

    Add fibionacci code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function myFun(position) {
+    if (position < 0)
+    {
+        return ("OOPS");
+    }
+    if (position == 0)
+    {
+        return 0;
+    }
+    if (position == 1 | position == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return myFun(position-1) + myFun(position-2);
+    }
 };
 
 // Do not edit below this line
