625ad6ed73ac9229f8805da5e2302a578e0b4fd3
Adnan Yusuf <adnanyusuf@outlook.com>
2024-08-09 07:41:00 +0500

    Add palindrome code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    let characters = sentence.toLowerCase().split("");
+    let alphaCharactersOnly = characters.filter(x=> {return x.match(/[a-z0-9]/);});
+    
+    let length = alphaCharactersOnly.length;
 
+    for (let i=0;i<length;i++)
+    {
+        console.log(`${alphaCharactersOnly[i]} and ${alphaCharactersOnly[length-i-1]}`);
+        if (alphaCharactersOnly[i] != alphaCharactersOnly[length-i-1])
+        {
+            return false;
+        }
+    }
+    return true;
+    
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
